d0ef8fbd299bb6c7bddf25030f43b84df1d5f680
Philip Moeller <philip.moeller@exoscale.com>
2025-08-14 13:37:09 +0200

    Clea up and changes to sks.tf for deployment

--- provider.tf
+++ provider.tf
@@ -1,7 +1,13 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     exoscale = {
       source  = "exoscale/exoscale"
     }
   }
+}
+
+provider "exoscale" {
+  key = "${var.exoscale_api_key}"
+  secret = "${var.exoscale_secret_key}"
 }
--- sks.tf
+++ sks.tf
@@ -1,10 +1,12 @@
 # Change version to the currently available Exoscale SKS version
-resource "exoscale_sks_cluster" "SKS-Cluster" {
+resource "exoscale_sks_cluster" "solace-ha" {
   zone          = local.zone
-  name          = "solace-ha"
+  name          = local.project
   version       = "1.32.6"
-  description   = "Cluster for Solace HA deployment"
+  description   = "Cluster for Solace HA deployment ${local.project}"
   service_level = "pro"
+  exoscale_ccm  = true
+  exoscale_csi  = true
 }
 
 # This provisions an instance pool of nodes which will run the Kubernetes
@@ -13,15 +15,16 @@ resource "exoscale_sks_cluster" "SKS-Cluster" {
 # Check instance types: https://www.exoscale.com/pricing/#/compute/
 
 resource "exoscale_anti_affinity_group" "solace-ha" {
-  name        = "solace-ha"
+  name        = "solace-${local.project}"
   description = "Prevent compute instances to run on the same host"
 }
 
 resource "exoscale_sks_nodepool" "solace-ha" {
   zone               = local.zone
-  cluster_id         = exoscale_sks_cluster.SKS-Cluster.id
-  name               = "solace-ha"
-  instance_type      = "standard.large"
+  cluster_id         = exoscale_sks_cluster.solace-ha.id
+  name               = local.project
+  instance_prefix    = local.project
+  instance_type      = "standard.extra-large"
   size               = 3
   security_group_ids = [exoscale_security_group.sks_nodes.id]
   anti_affinity_group_ids = [exoscale_anti_affinity_group.solace-ha.id]
@@ -30,7 +33,7 @@ resource "exoscale_sks_nodepool" "solace-ha" {
 # Create a security group so the nodes can communicate and we can pull logs
 
 resource "exoscale_security_group" "sks_nodes" {
-  name        = "sks_nodes"
+  name        = "sg-${local.project}"
   description = "Allows traffic between sks nodes and public pulling of logs"
 }
 
@@ -62,4 +65,18 @@ resource "exoscale_security_group_rule" "sks_nodes_ccm" {
   end_port          = 32767
   description       = "NodePort services"
   cidr              = "0.0.0.0/0"
+}
+
+resource "exoscale_sks_kubeconfig" "solace-ha_admin" {
+  ttl_seconds = 360000
+  early_renewal_seconds = 300
+  cluster_id = exoscale_sks_cluster.solace-ha.id
+  zone = exoscale_sks_cluster.solace-ha.zone
+  user = "solace-ha-admin"
+  groups = ["system:masters"]
+}
+
+resource "local_file" "kube_config" {
+  content = exoscale_sks_kubeconfig.solace-ha_admin.kubeconfig
+  filename = "kube_config"
 }
